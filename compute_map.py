fb7a1538f07dfd474ba8cf81e709979cddd0c9ff
Simonlyym <1578734103@qq.com>
2020-07-14 02:19:48 +0000

    face mask

--- compute_map.py
+++ compute_map.py
@@ -1,20 +1,20 @@
 from voc_eval import voc_eval
 import numpy as np
 # hat
-print("==========Hat==========")
+print("==========with_mask==========")
 Hrec, Hpre, Hap = voc_eval('./results/{}.txt', 
                './VOCdevkit/VOC2007/Annotations/{}.xml', 
-               '/content/colab_darknet_yolo/2007_test.txt', 
-               'hat', '.')
+               '/content/colab_face_mask/2007_test.txt', 
+               'with_mask', '.')
 print("rec:", round(np.mean(Hrec), 4))
 print("pre:", round(np.mean(Hpre), 4))
 print(" ap:", round(np.mean(Hap), 4))
 # person
-print("========person=========")
+print("========without_mask=========")
 Prec, Ppre, Pap = voc_eval('./results/{}.txt', 
                './VOCdevkit/VOC2007/Annotations/{}.xml', 
-               '/content/colab_darknet_yolo/2007_test.txt', 
-               'person', '.')
+               '/content/colab_face_mask/2007_test.txt', 
+               'without_mask', '.')
 print("rec:", round(np.mean(Prec), 4))
 print("pre:", round(np.mean(Ppre), 4))
 print(" ap:", round(np.mean(Pap), 4))
